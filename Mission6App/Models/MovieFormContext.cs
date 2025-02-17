aab8e8014c38d6dc30cbff877ba6a7676a7d8c53
dmattern14 <130599602+dmattern14@users.noreply.github.com>
2025-02-17 09:33:38 -0700

    all changes for Mission 6

--- Mission6App/Models/MovieFormContext.cs
+++ Mission6App/Models/MovieFormContext.cs
@@ -1,6 +1,22 @@
-namespace Mission6App.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
-public class MovieFormContext
+namespace Mission6App.Models
 {
-    
+    public class MovieFormContext : DbContext
+    {
+        public MovieFormContext(DbContextOptions<MovieFormContext> options) : base(options) //constructor
+        {
+        } 
+        public DbSet<Application> Forms { get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Application>()
+                .Property(a => a.Rating)
+                .HasConversion(new EnumToStringConverter<MovieRating>()); // ✅ Converts enum to string in DB
+        }
+    }
 }
+
+
--- Mission6App/Program.cs
+++ Mission6App/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Mission6App.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<MovieFormContext>(options =>
+{
+    options.UseSqlite(builder.Configuration["ConnectionStrings:MovieFormConnection"]);
+});
 
 var app = builder.Build();
 
